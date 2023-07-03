0e234fc2798004e9b8b255b47926c80117e96636
Komplete101 <kylenwosu2003@gmail.com>
2023-07-02 20:47:41 -0700

    More in depth web scrapper

---
+++ Yahoofin-scrapper.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul  2 20:26:51 2023
+
+@author: Chiboy
+"""
+
+from yahoofinancials import YahooFinancials
+
+stock = "TSLA"
+yahoo_financials = YahooFinancials(stock)
+data  = yahoo_financials.get_historical_price_data("2020-04-28", "2023-07-2","daily")                                   
+#provide start date end date and period 
