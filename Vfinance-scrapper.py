ea62a049f3c78c349e66ea223420fae814b3e88b
Komplete101 <kylenwosu2003@gmail.com>
2023-07-02 19:16:17 -0700

    Learned the specific basics on how to use the webscrapper

--- Vfinance-scrapper.py
+++ Vfinance-scrapper.py
@@ -2,14 +2,20 @@
 import datetime as dt
 import pandas as pd
 import yfinance as yf
-data = yf.download("MSFT", period = "1mo", interval="5m")
+#data = yf.download("MSFT", period = "1mo", interval="5m")
 #Can be data = yf.download("Stock abreviations", start = "yyyy-mm-dd", end = "yyyy-mm-dd")
 
 
-stocks = ["AMZN","TSLA",'MSFT',"GOOG"]
-start = dt.datetime.today()-datetime.timedelta(30)
+stocks = ["AMZN","TSLA",'MSFT',"GOOG","GM","INTC","PG"]
+start = dt.datetime.today()-dt.timedelta(360)
 end = dt.datetime.today()
 cl_price = pd.DataFrame()
 
-for ticker in stocks:
-    cl_price[ticker] = yf.download(ticker,start,end)["Adj close'"]
+alldata = {}
+#Loops over the specific stocks
+for stock in stocks:
+    cl_price[stock] = yf.download(stock,start,end)["Adj Close"]
+#This loops through every stock in stocks and add the data in adjusted close
+
+for stock in stocks:
+    alldata[stock] = yf.download(stock,start,end)
