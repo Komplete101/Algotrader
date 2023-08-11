747ab4b2ba16e19323e2f8c5a6b032739ef1af1c
Komplete101 <kylenwosu2003@gmail.com>
2023-08-10 21:58:06 -0400

    Accessing info from the heading

--- Html Webscraper.py
+++ Html Webscraper.py
@@ -8,18 +8,24 @@ Created on Sat Jul 15 14:55:22 2023
 import requests
 from bs4 import BeautifulSoup
 
-
-
-
+stocks = ["AAPL","MSFT","NVDA","INTC", "AMD"]
 income_statement = {}
-url = 'https://finance.yahoo.com/quote/AAPL/financials?p=AAPL&guccounter=1'
-headers = {'User-Agent': "Chrome/114.0.5735.199 "}
-page = requests.get(url, headers=headers)
-page_content = page.content
-soup = BeautifulSoup(page_content, "html.parser")
-table = soup.find_all("div", {"class": "M(0) Whs(n) BdEnd Bdc($seperatorColor) D(itb)"})
-for t in table:
-    rows = t.find_all("div", {"class": "D(tbr) fi-row Bgc($hoverBgColor):h"})
-    for row in rows:
-        income_statement[row.get_text(separator= " | ").split("|")[0]] = row.get_text(separator= " | ").split(" | ")[1]
-        
+balance_sheet_dict = {}
+cashflow = {}
+
+for stock in stocks:
+
+    url = 'https://finance.yahoo.com/quote/{}/financials?p={}'.format(stock)
+    income = {}
+    table_title = {}
+    
+    headers = {'User-Agent': "Chrome/115.0.5790.171"}
+    page = requests.get(url, headers=headers)
+    page_content = page.content
+    soup = BeautifulSoup(page_content, "html.parser")
+    table = soup.find_all("div", {"class": "M(0) Whs(n) BdEnd Bdc($seperatorColor) D(itb)"})
+    for t in table:
+        heading = t.find_all("div", {"class": "D(tbr) C($primaryColor) D(itb)"})
+        for top_row in heading:
+            table_title[top_row.get_text(separator= "|").split("|")[0]] = top_row.get_text(separator = "|").split("|")[1:]
+            
