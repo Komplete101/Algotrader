a8b995d54ef8aec7d31ffdfeb80022b8572ae19b
Komplete101 <kylenwosu2003@gmail.com>
2023-08-11 09:35:35 -0400

    Update Html Webscraper.py
    Expanded the script to run on different pages

--- Html Webscraper.py
+++ Html Webscraper.py
@@ -7,16 +7,17 @@ Created on Sat Jul 15 14:55:22 2023
 
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
 stocks = ["AAPL","MSFT","NVDA","INTC", "AMD"]
-income_statement = {}
+income_statement_dict= {}
 balance_sheet_dict = {}
-cashflow = {}
+cashflow_dict= {}
 
 for stock in stocks:
 
-    url = 'https://finance.yahoo.com/quote/{}/financials?p={}'.format(stock)
-    income = {}
+    url = 'https://finance.yahoo.com/quote/{}/financials?p={}&guccounter=1'.format(stock,stock)
+    income_statement = {}
     table_title = {}
     
     headers = {'User-Agent': "Chrome/115.0.5790.171"}
@@ -25,7 +26,59 @@ for stock in stocks:
     soup = BeautifulSoup(page_content, "html.parser")
     table = soup.find_all("div", {"class": "M(0) Whs(n) BdEnd Bdc($seperatorColor) D(itb)"})
     for t in table:
-        heading = t.find_all("div", {"class": "D(tbr) C($primaryColor) D(itb)"})
+        heading = t.find_all("div", {"class": "D(tbr) C($primaryColor)"})
         for top_row in heading:
             table_title[top_row.get_text(separator= "|").split("|")[0]] = top_row.get_text(separator = "|").split("|")[1:]
+        rows = t.find_all("div", {"class", "D(tbr) fi-row Bgc($hoverBgColor):h"})
+        for row in rows:
+            income_statement[row.get_text(separator = "|").split("|")[0]] = row .get_text(separator = "|").split("|")[1:]  
             
+    temp = pd.DataFrame(income_statement).T
+    temp.columns = table_title["Breakdown"]
+    income_statement_dict[stock] = temp
+
+for stock in stocks:
+
+    url = 'https://finance.yahoo.com/quote/{}/cash-flow?p={}&guccounter=1'.format(stock,stock)
+    cashflow = {}
+    table_title = {}
+    
+    headers = {'User-Agent': "Chrome/115.0.5790.171"}
+    page = requests.get(url, headers=headers)
+    page_content = page.content
+    soup = BeautifulSoup(page_content, "html.parser")
+    table = soup.find_all("div", {"class": "M(0) Whs(n) BdEnd Bdc($seperatorColor) D(itb)"})
+    for t in table:
+        heading = t.find_all("div", {"class": "D(tbr) C($primaryColor)"})
+        for top_row in heading:
+            table_title[top_row.get_text(separator= "|").split("|")[0]] = top_row.get_text(separator = "|").split("|")[1:]
+        rows = t.find_all("div", {"class", "D(tbr) fi-row Bgc($hoverBgColor):h"})
+        for row in rows:
+            cashflow[row.get_text(separator = "|").split("|")[0]] = row .get_text(separator = "|").split("|")[1:]  
+            
+    temp = pd.DataFrame(cashflow).T
+    temp.columns = table_title["Breakdown"]
+    cashflow_dict[stock] = temp
+    
+for stock in stocks:
+
+    url = 'https://finance.yahoo.com/quote/{}/balance-sheet?p={}&guccounter=1'.format(stock,stock)
+    balance_sheet = {}
+    table_title = {}
+    
+    headers = {'User-Agent': "Chrome/115.0.5790.171"}
+    page = requests.get(url, headers=headers)
+    page_content = page.content
+    soup = BeautifulSoup(page_content, "html.parser")
+    table = soup.find_all("div", {"class": "M(0) Whs(n) BdEnd Bdc($seperatorColor) D(itb)"})
+    for t in table:
+        heading = t.find_all("div", {"class": "D(tbr) C($primaryColor)"})
+        for top_row in heading:
+            table_title[top_row.get_text(separator= "|").split("|")[0]] = top_row.get_text(separator = "|").split("|")[1:]
+        rows = t.find_all("div", {"class", "D(tbr) fi-row Bgc($hoverBgColor):h"})
+        for row in rows:
+            balance_sheet[row.get_text(separator = "|").split("|")[0]] = row .get_text(separator = "|").split("|")[1:]  
+            
+    temp = pd.DataFrame(balance_sheet).T
+    temp.columns = table_title["Breakdown"]
+    balance_sheet_dict[stock] = temp
